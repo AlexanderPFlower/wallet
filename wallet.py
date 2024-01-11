d8e3b8810eb8380b3a5e2ed69e0e71e87cc5ce05
Alexander Flower <142947266+AlexanderSmith0@users.noreply.github.com>
2024-01-11 20:26:52 +1000

    Update wallet.py by removing some comments

--- wallet.py
+++ wallet.py
@@ -1,27 +1,3 @@
-'''
-What works:
-1. I can get the user to give me numbers for each money denomination aka moneyType, stored in benchtop
-2. I can then access the values of moneyValues and store that in a list, as well as accessing benchtop values
-3. I can take these two lists and zip them together to do a 'for f, b in zip(list1, list2)' in order to create a
-new list. The actual code is below, where counter is counting each 'physical' coin or note, and v is a placeholder
-for 'values' aka moneyValues (the float value of centage and dollar representation).
-
-for amount, financialValue in zip(counter, v):
-    x = amount * financialValue
-    wallet.append(x)
-
-What I'm trying to do now:
-NOW I FINALLY KNOW HOW TO UPDATE THE WALLET! HUZZAH!
-The next step is to finish the wallet.
-I noticed that having 3 five cents results in 0.15000000000000002.
-I will be testing the current state with values all 1, then 2, etc
-
-Okay so multiples of 3, 6, and 7 do not work for 0.05, 0.10, and 0.20 centages.
-
-
-HUZZAH! It is called precision handling using modulo %.
-%5.4f % 3.14592 = 3.1416 because 5 = the # of numbers, 4 is # of numbers post-decimal, and it is rounded up?
-'''
 import math
 
 # named denomination of currencies in Australia
@@ -54,43 +30,6 @@ for i in moneyTypes:
 # hashtag 'took me all day but I learnt how to do it!' It's so clean and simple, too!
 walletCount = {moneyTypes: benchtop for moneyTypes, benchtop in zip(moneyTypes, benchtop)}
 
-# REFERENCES FOR ACCESSING KEYS AND VALUES
-'''
-# Reference for printing each item in moneyTypes and values of moneyValues.values()
-
-def playerWallet():
-    for i in moneyTypes:
-        print(i)
-    for i in moneyValues.values():
-        print(i)
-
-# Reference for these two data sets into separate lists
-
-def playerWallet():
-    k = []
-    v = []
-    for i in moneyTypes:
-        k.append(i)
-    for i in moneyValues.values():
-        v.append(i)
-    print(k)
-    print(v)
-
-
-def checker():
-    # Creating a list of all the values in moneyValues
-    v = []
-    for i in moneyValues.values():
-        v.append(i)
-
-    temp = []
-    for i in benchtop:
-        temp.append(i)
-    
-    print("\nHere is a list of all the values in moneyValues!\n" + str(v))
-    print("And here are the frequencies of each value we have on the benchtop lol\n" + str(temp))
-'''
-
 def playerWallet():
     # Accessing values
     v = []
@@ -107,11 +46,9 @@ def playerWallet():
         x = amount * financialValue
         x = "%.2f" % x
         wallet.append(x)
-    
-    
+        
     # Test printing result
     print(wallet)
-
 
 
 playerWallet()
